8e913c18e3ef4d128c2264d05822479c2a10b3a5
JASHANDEEP2608 <jashandeepgarry2003@gmail.com>
2024-12-03 03:16:32 +0530

    Update Render Depssssp

--- image_crawler_project/settings.py
+++ image_crawler_project/settings.py
@@ -27,7 +27,7 @@ import os
 
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true' 
  # Use environment variable
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ '*','image_crawler.onrender.com' ]
 
 
 
